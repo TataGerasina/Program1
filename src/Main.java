ead0f2193bc74317efb2e54fa1e35f06f2c0e5bd
Tatyana Gerasina <gtstata@gmail.com>
2024-01-13 17:46:23 +0300

    renamed variables

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int price = 50_000;
-        int rubles_mile = 20;
+        int rublesMile = 20;
 
-        int bonus_mile = price / rubles_mile;
-        System.out.println("Количество начисленных миль за купленный билет: " + bonus_mile);
-        
+        int bonusMile = price / rublesMile;
+        System.out.println("Количество начисленных миль за купленный билет: " + bonusMile);
+
     }
 }
